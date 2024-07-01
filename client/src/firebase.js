4fd3320966a9789afbf1321c7035c4266f319bbd
BEENA-PAL <129384052+BEENA-PAL@users.noreply.github.com>
2024-07-01 08:11:23 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAM2AmogNYqxzHpWzZZqJtxTvE2APGJuEc",
+  authDomain: "sahandsblogapp.firebaseapp.com",
+  projectId: "sahandsblogapp",
+  storageBucket: "sahandsblogapp.appspot.com",
+  messagingSenderId: "531679959383",
+  appId: "1:531679959383:web:19567b1fc11502f7c08612",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
